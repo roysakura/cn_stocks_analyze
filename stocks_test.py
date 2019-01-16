19cb56303d7ae9f6eb37d4f02319abd96142c1d3
Roy <roy.liang@ectrend.com>
2019-01-16 21:26:12 +0800

    update

--- stocks_test.py
+++ stocks_test.py
@@ -9,9 +9,29 @@ from progressbar import ProgressBar,SimpleProgress,Bar,ETA,ReverseBar
 import settings
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
+import sqlite3
 
 
 # connect to the database
-conn = create_engine(URL(**settings.DATABASE))
-df = pd.read_sql("SELECT * from \'002684\';",conn)
-print((df))
+conn = sqlite3.connect('cn_stocks.db')
+cur = conn.cursor()
+
+all_stocks = ts.get_stock_basics()
+all_stocks_list = all_stocks.index.tolist()
+
+widgets = [Bar('>'), ' ', ETA(), ' ', ReverseBar('<')]
+pbar = ProgressBar(widgets=widgets,maxval=len(all_stocks_list)).start()
+for i,code in enumerate(all_stocks_list):
+		try:
+			cur.execute('DELETE FROM \'{}\' WHERE date=\'20190116\''.format(code))
+			cur.commit()
+		except Exception as e:
+			print(e)
+			continue
+		pbar.update(i + 1)
+
+pbar.finish()
+conn.close()
+
+
+
