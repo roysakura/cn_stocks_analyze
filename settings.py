a7b89c736e5e6c0927db2be224f4182dc86c9db5
Roy <roy.liang@ectrend.com>
2019-01-22 19:42:29 +0800

    update

--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ DATABASE = {
     'drivername': 'mysql',
     'host': 'localhost',
     'port': '3306',
-    'username': 'stock',
-    'password': '494904',
+    'username': '',
+    'password': '',
     'database': 'cnstock',
     'query': {'charset': 'utf8'},
 }
