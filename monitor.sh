703d7e64e80e1c63a3dbf76b510929ad5dbabd39
Roy <roy.liang@ectrend.com>
2019-03-29 14:52:12 +0800

    update

--- monitor.sh
+++ monitor.sh
@@ -3,3 +3,7 @@ dir1=/workspace/projects/cn_stocks_analyze/data/concepts/
 while inotifywait -qqre modify "$dir1"; do
 	cd /workspace/projects/cn_stocks_analyze/ && /workspace/projects/cn_stocks_analyze/venv/bin/python concept_updates.py 
 done
+dir1=/workspace/projects/cn_stocks_analyze/data/industry/
+while inotifywait -qqre modify "$dir1"; do
+	cd /workspace/projects/cn_stocks_analyze/ && /workspace/projects/cn_stocks_analyze/venv/bin/python industry_updates.py 
+done
